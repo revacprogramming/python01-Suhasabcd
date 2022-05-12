312c83da62d5ea9441c1860ea3d878b39a8536c9
Suhasabcd <suhasknightyt@gmail.com>
2022-05-12 23:07:33 +0530

    tuple

--- ActivitySet01/problem12.py
+++ ActivitySet01/problem12.py
@@ -1,2 +1,21 @@
-# Regular Expressions
-# https://www.py4e.com/lessons/regex
+name = "mbox-short.txt"
+dic=list()
+a=list()
+n=list()
+fh=open(name)
+c=fh.readlines()
+for i in c :
+    k=i.split(" ")
+    if i.find("From ") != -1:
+        if k[5] not in dic:
+            dic.append(k[6])
+for i in dic :
+    b=i.split(":")
+    a.append(b[0])
+                            
+           
+a.sort() 
+for i in a:
+    if i not in n:
+        print(i,a.count(i))
+        n.append(i)
