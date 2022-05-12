150dfed03909715aeb3470ca6287b74de4eb0c63
Suhasabcd <suhasknightyt@gmail.com>
2022-05-12 22:37:38 +0530

    dictonaries

--- ActivitySet01/problem11.py
+++ ActivitySet01/problem11.py
@@ -1,3 +1,17 @@
-# Tuples
-
-filename = "dataset/mbox-short.txt"
+name = "mbox-short.txt"
+dic={ }
+fh=open(name)
+c=fh.readlines()
+for i in c :
+    k=i.split()
+    if i.find("From ") != -1:
+        if k[1] not in dic:
+            dic[k[1]]=0
+for i in c :
+    k=i.split()
+    if i.find("From ") != -1:
+        if k[1] in dic:
+            dic[k[1]]=dic[k[1]]+1
+v=list(dic.values())
+keys=list(dic.keys())
+print(keys[v.index(max(v))],max(v))
