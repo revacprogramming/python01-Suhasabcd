1c8253daa8f759f255244ed7bb95cfc1b58cab6f
Suhasabcd <suhasknightyt@gmail.com>
2022-05-16 09:34:20 +0530

    regular expression

--- ActivitySet01/problem13.py
+++ ActivitySet01/problem13.py
@@ -1,2 +1,9 @@
-# Network Programming
-# https://www.py4e.com/lessons/network
+import re
+file_name="actualdata.txt"
+f=open(file_name,"r")
+c=f.read()
+sum=0
+num=re.findall("[0-9]+",c)
+for i in num:
+    sum=sum+int(i)
+print(sum)
