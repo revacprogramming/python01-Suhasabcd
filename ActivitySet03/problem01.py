99b13f8e1614ef764e8e8a3f592756d2402292e6
Suhasabcd <suhasknightyt@gmail.com>
2022-05-19 22:39:09 +0530

    activit 3 q 1

---
+++ ActivitySet03/problem01.py
@@ -0,0 +1,8 @@
+t=int(input())
+for i in range(t):
+    x=list(map(int,input().split()))
+    x1,y1=x[0],x[1]
+    x2,y2=x[2],x[3]
+    x3,y3=x[4],x[5]
+    area=(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2))
+    print(area)
