001e24ef7fca38585e04cb3c2b60416ef1e72ff5
Suhasabcd <suhasknightyt@gmail.com>
2022-05-21 19:39:31 +0530

    activity 3 problem 2

---
+++ ActivitySet03/problem02.py
@@ -0,0 +1,22 @@
+l=[]
+t=int(input())
+for i in range(t):
+    hcf=1
+    n=input()
+    x=list(map(int,input().split()))
+    sum_num=1
+    sum_den=x[0]
+    print(f"1/{x[0]} ",end="")
+    for i in range(1,len(x)):
+        print("+ ",end="")
+        print(f"1/{x[i]}",end="")
+        sum_num=(sum_den+sum_num*x[i])
+        sum_den=(sum_den*x[i])
+    for i in range(1, min(sum_den, sum_num)):
+
+        if sum_num % i == 0 and sum_den % i == 0:
+            hcf = i    
+        
+    sum_num=sum_num/hcf
+    sum_den=sum_den/hcf
+    print(f"= {int(sum_num)}/{int(sum_den)}")
