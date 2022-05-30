fe5c1d0e847c83ad0e60490a7f95f74d5c327e94
Suhasabcd <suhasknightyt@gmail.com>
2022-05-30 08:37:17 +0530

    set 3 problem 3

--- ActivitySet03/problem03.py
+++ ActivitySet03/problem03.py
@@ -5,9 +5,8 @@ for _ in range(int(input())):
     x=input()
     for i in range(y):
         for j in range(1,y):
-            if(len(x[i:j+1])>2):
-                if isPalindrome(x[i:j+1]):
-                    print(i+1,x[i:j+1])
+            if(len(x[i:j+1])>2 and isPalindrome(x[i:j+1])):
+                print(i+1,x[i:j+1])
                     
             
                 
